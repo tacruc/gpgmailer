9356792b0fbbaa56cd8292289b5c9289918de211
Arne Hamann <kontakt+github@arne.email>
2019-03-18 00:02:44 +0100

    Cleanup
    Signed-off-by: Arne Hamann <kontakt+github@arne.email>

---
+++ tests/Unit/Controller/KeyControllerTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace OCA\GpgMailer\Tests\Unit\Controller;
+
+
+use PHPUnit_Framework_TestCase;
+
+use OCP\AppFramework\Http\TemplateResponse;
+use OCA\GpgMailer\Controller\KeyController;
+
+
+class KeyControllerTest extends PHPUnit_Framework_TestCase {
+	private $controller;
+	private $userId = 'john';
+
+
+	public function setUp() {
+		$request = $this->getMockBuilder('OCP\IRequest')->getMock();
+		$userManager = $this->getMockBuilder('OCP\IUserManager')->getMock();
+		$gpg = $this->getMockBuilder('OCA\GpgMailer\Gpg')->getMock();
+		$config = $this->getMockBuilder('OCP\IConfig')->getMock();
+		$this->controller = new KeyController("gpgmailer",$request, $gpg, $config, $userManager, $this->userId);
+	}
+
+	public function testDownloadServerKey() {
+		$result = $this->controller->downloadServerKey();
+		
+	}
+
+}
